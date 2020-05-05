e0a0065e0cccf39316da9b534e089feb6d5028d7
Fabio Muller <fabio.muller@fael.edu.br>
2020-05-05 09:40:25 -0300

    all css included

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,3 +1,10 @@
 function mudarFoto(foto) {
   document.getElementById("icone").src = `./_imagens/${foto}.png`;
 }
+
+function calcTotal() {
+  let qtd = parseInt(document.getElementById("qtd").value)
+  let total = qtd * 1500
+
+  document.getElementById("total").value = total
+}
